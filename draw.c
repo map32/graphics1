3ed07c8ec86c3f02e8de9f3844d24eef2a76d372
map32 <freepina@gmail.com>
2016-02-11 13:19:42 -0500

    a

---
+++ draw.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+
+//Insert your line algorithm here
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int dx,dy;
+  int x,y;
+  x = x0;
+  y = y0;
+  A = x1-x0;
+  B = -(y1-y0);
+  if (A>=0){
+    if (B)
+  }
+}
+
+y = mx+b
+0 = mx-y+b
+
+  0 = dx/dy(x)-y+b
+  0 = dx(x)+(-dy)y+(dy)b
