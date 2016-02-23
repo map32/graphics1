99abde09f7fcc60c9d6a02023352bf2cbbe0cdcf
freepina <freepina@gmail.com>
2016-02-23 18:38:46 -0500

    hi!

--- draw.c
+++ draw.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "ml6.h"
 #include "display.h"
 #include "draw.h"
 
-void plot2(int flip, screen s, color c, int always, int conditional){
-  if (!flip){
-    plot(s,c,always,conditional);
-  } else {
-    plot(s,c,conditional,always);
-  }
-}
-
 void draw2(int always0, int conditional0, int always1, int flip, int dalways, int dconditional, int talways, int tconditional, screen s, color c){
   int D = talways+tconditional/2;
-  printf("%d %d %d",talways,tconditional,D);
+  //printf("%d %d %d",talways,tconditional,D);
   int always = always0;
   int conditional = conditional0;
   if(!flip){
@@ -28,6 +21,7 @@ void draw2(int always0, int conditional0, int always1, int flip, int dalways, in
       always += dalways;
       D += talways;
     }
+    plot(s,c,always,conditional);
   } else {
     while(always!=always1){
       plot(s,c,conditional,always);
@@ -38,6 +32,7 @@ void draw2(int always0, int conditional0, int always1, int flip, int dalways, in
       always += dalways;
       D += talways;
     }
+    plot(s,c,conditional,always);
   }
 }
 
@@ -46,35 +41,98 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   int dx,dy;
   int A = 2*(y1-y0);
   int B = 2*(-(x1-x0));
-  printf("d");
+  //printf("d\n");
   if (A<0){ //5-8 octants flipped into 1-4
-    int t = y1;
-    y1 = y0;
-    y0 = t;
-    t = x1;
+    int t = x1;
     x1 = x0;
     x0 = t;
-    printf("%d %d",x0,x1);
-    A = -A;
+    t = y1;
+    y1 = y0;
+    y0 = t;
+    //printf("%d %d %d %d\n",x0,x1,y0,y1);
     B = -B;
+    A = -A;
   }
   if (A>B && A>-B){ // 2,3
     if (B<0){ // 2
-      printf("2");
+      //printf("2");
       //draw2(x0,y0,x1,0,1,1,A,B,s,c);
       draw2(y0,x0,y1,1,1,1,B,A,s,c);
     } else { // 3
-      printf("3");
-      draw2(y0,x0,y1,1,1,-1,B,A,s,c);
+      //printf("3");
+      draw2(y0,x0,y1,1,1,-1,-B,A,s,c);
     }
   } else { //1,4
     if (B<0){ // 1
-      plot(s,c,100,200);
-      printf("1\n");
+      //printf("1\n");
       draw2(x0,y0,x1,0,1,1,A,B,s,c);
     } else { // 4
-      printf("4f");
-      draw2(x0,y0,x1,0,-1,1,A,B,s,c);
+      //printf("4\n");
+      draw2(x0,y0,x1,0,-1,1,A,-B,s,c);
     }
   }
+}
+
+void rectangle(int x0, int y0, int x1, int y1, screen s, color c){
+  draw_line(x0,y0,x1,y0,s,c);
+  draw_line(x1,y0,x1,y1,s,c);
+  draw_line(x1,y1,x0,y1,s,c);
+  draw_line(x0,y1,x0,y0,s,c);
+}
+
+void radiance(int x0, int y0, int x1, int y1, screen s, color c){
+  int q = 0; // 0:normal 1:yflip 2:xflip 3:xyflip
+  if (x0<x1){
+    if (y0<y1){
+    } else {
+      q = 1;
+    }
+  } else {
+    if (y0<y1){
+      q = 2;
+    } else {
+      q = 3;
+    }
+  }
+  int i=0;
+  for(i;i<8;i++){
+    if (q>0){
+      y0 += 1;
+      y1 -= 1;
+    } else {
+      y0 -= 1;
+      y1 += 1;
+    }
+    if (q>1){
+      x0 += 1;
+      x1 -= 1;
+    } else {
+      x0 -= 1;
+      x1 += 1;
+    }
+    c.red = c.red*4/5;
+    c.blue = c.blue*4/5;
+    c.green = c.green*4/5;
+    rectangle(x0,y0,x1,y1,s,c);
+  }
+}
+
+void randomy(screen s){
+  color c;
+  srand(time(NULL));
+  int x;
+  int y;
+  c.red = rand()%256;
+  c.green = rand()%256;
+  c.blue = rand()%256;
+  int i = 0;
+  for(i;i<50;i++){
+    x = rand()%XRES;
+    y = rand()%YRES;
+    c.red = rand()%256;
+    c.green = rand()%256;
+    c.blue = rand()%256;
+    radiance(x,y,x+20,y+20,s,c);
+  }
+  
 }
--- main.c
+++ main.c
@@ -20,12 +20,14 @@ int main() {
 
 
   //octant 1
+  //draw_line( 0, 0, YRES-75, XRES-1,s,c);
   //draw_line( 0, 0,100, 50, s, c);
   //octant 2
+  //draw_line(0,0,YRES-1,XRES-75,s,c);
   //draw_line( 0, 0, 50, 100, s, c); 
   //octant 8
   //draw_line( 0, YRES-1, XRES-1, 75, s, c);
-  draw_line(250,250,499,200,s,c);
+  //draw_line(250,250,499,200,s,c);
   //draw_line( 0, YRES-1, XRES-1, 75, s, c);
   //octant 7
   //draw_line( 0, YRES-1, XRES - 75, 0, s, c);
@@ -49,7 +51,27 @@ int main() {
 
   //horizontal, vertical line
   //draw_line( 0, YRES / 2, XRES - 1, YRES / 2, s, c);
-  //udraw_line( XRES / 2, 0, XRES / 2, YRES - 1, s, c);
+  //draw_line( XRES / 2, 0, XRES / 2, YRES - 1, s, c);
+
+  randomy(s);
+  
+  c.red = 255;
+  c.blue = 180;
+  c.green = 105;
+  radiance(70,100,90,400,s,c);
+  radiance(100,250,190,270,s,c);
+  radiance(200,270,220,100,s,c);
+  c.red = 255;
+  c.blue = 203;
+  c.green = 193;
+  radiance(280,100,300,350,s,c);
+  radiance(280,380,300,400,s,c);
+  c.red = 255;
+  c.blue = 147;
+  c.green = 20;
+  radiance(340,100,360,120,s,c);
+  radiance(340,140,360,400,s,c);
+  //radiance(300,400,100,100,s,c);
   
   display(s);
   save_extension(s, "lines.png");
